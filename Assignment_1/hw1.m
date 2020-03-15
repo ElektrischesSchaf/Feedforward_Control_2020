16e1369af70d8afd6f18ce13a06803db4e3189f3
ElektrischesSchaf <vertigo1958psycho1960@gmail.com>
2020-03-15 21:34:01 +0800

    hw1

---
+++ Assignment_1/hw1.m
@@ -0,0 +1,12 @@
+%% initialize the system
+numerator=[11.88 4.977 539.6 129.9 5625]
+denominator=[1 1.169 50.3 45.94 685.3 391.7 1952]
+sys=tf(numerator, denominator);
+
+%% Bode Plot of the system
+bode(sys); % change frequency unit to KHz in "View" -> "Property Editor"
+
+%% Step response of the system
+plot(step(sys));
+
+
