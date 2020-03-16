d452936ecae81aaffe42110f12351cc94626b33f
ElektrischesSchaf <vertigo1958psycho1960@gmail.com>
2020-03-16 20:25:03 +0800

    no ode45

--- Assignment_1/hw1.m
+++ Assignment_1/hw1.m
@@ -1,8 +1,9 @@
+clc;
 clear;
 %% initialize the system
 numerator=[11.88 4.977 539.6 129.9 5625]
 denominator=[1 1.169 50.3 45.94 685.3 391.7 1952]
-sys=tf(numerator, denominator);
+Origianl_System=tf(numerator, denominator);
 
 %% Bode Plot of the system
 % bode(sys); % change frequency unit to KHz in "View" -> "Property Editor"
@@ -14,11 +15,11 @@ sys=tf(numerator, denominator);
 % relative degree =  6-4 =2
 
 %% Original System State Space
-csys= canon(sys,'companion')
-A=csys.A;
-B=csys.B;
-C=csys.C;
-D=csys.D;
+Original_System_State_Space= canon(Origianl_System,'companion')
+A=Original_System_State_Space.A;
+B=Original_System_State_Space.B;
+C=Original_System_State_Space.C;
+D=Original_System_State_Space.D;
 
 %% inverse system State Space
 A_inv=A-((B*C*A*A)/(C*A*B));
@@ -26,7 +27,7 @@ B_inv=(B)/(C*A*B);
 C_inv=(-C*A*A)/(C*A*B);
 D_inv=1/(C*A*B);
 
-%% signal generation
+%% U_ff signal generation
 % https://electrosome.com/signal-generation-in-matlab/
 n1=0; n2=10;
 t=n1:0.1:n2;
@@ -34,9 +35,31 @@ x1=1*[t>=0];
 x2=-2*[t>=1];
 x3=2*[t>=3];
 x4=-1*[t>=4];
-input=x1+x2+x3+x4;
+U_ff=x1+x2+x3+x4;
 % plot(t,input); % stem(n,x);
 % title('Unit Step Signal - Cont');
 
-inverse_system=ss(A_inv, B_inv, C_inv, D_inv);
-lsim(inverse_system, input, t);
+%% Inverse System
+Inverse_System_State_Space=ss(A_inv, B_inv, C_inv, D_inv);
+%% 
+yd=lsim(Inverse_System_State_Space, U_ff, t);
+plot(U_ff);
+hold on
+plot(yd);
+
+%%
+y_position=lsim(Original_System_State_Space, yd, t);
+hold on
+plot(y_position);
+
+%% Differentiate y_position twice into y_acceleration
+for i=1:length(t)-1
+    y_velocity(i)=y_position(i+1)-y_position(i);
+end
+
+for i=1:length(t)-2
+    y_acceleration(i)=y_velocity(i+1)-y_velocity(i);
+end
+clf;
+plot(y_acceleration);
+% legend('yd','voltage input','y_position', 'y_acceleration');
