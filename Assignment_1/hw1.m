9973af85c9f063f287db52ab434379c0e2c4fadd
ElektrischesSchaf <vertigo1958psycho1960@gmail.com>
2020-03-17 15:33:59 +0800

    PID feedback

--- Assignment_1/hw1.m
+++ Assignment_1/hw1.m
@@ -1,5 +1,6 @@
 clc;
 clear;
+LineWidth=3;
 %% initialize the system
 numerator=[11.88 4.977 539.6 129.9 5625]
 denominator=[1 1.169 50.3 45.94 685.3 391.7 1952]
@@ -34,22 +35,21 @@ x1=1*[t>=0];
 x2=-2*[t>=1];
 x3=2*[t>=3];
 x4=-1*[t>=4];
-U_ff=x1+x2+x3+x4;
+yd=x1+x2+x3+x4;
 % plot(t,input); % stem(n,x);
 % title('Unit Step Signal - Cont');
 
 %% Inverse System
 Inverse_System_State_Space=ss(A_inv, B_inv, C_inv, D_inv);
 %% 
-yd=lsim(Inverse_System_State_Space, U_ff, t);
-plot(U_ff);
+U_ff=lsim(Inverse_System_State_Space, yd, t);
+plot(U_ff, 'LineWidth', LineWidth);
 hold on
-plot(yd);
+plot(yd, 'LineWidth', LineWidth);
 
 %%
-y_position=lsim(Original_System_State_Space, yd, t);
-hold on
-plot(y_position);
+y_position=lsim(Original_System_State_Space, U_ff, t);
+% plot(y_position);
 y_position=100.*y_position;
 
 %% Differentiate y_position twice into y_acceleration
@@ -61,5 +61,63 @@ for i=1:length(t)-2
     y_acceleration(i)=y_velocity(i+1)-y_velocity(i);
 end
 % clf;
-plot(y_acceleration);
-legend('yd','voltage input','y position', 'y acceleration');
+plot(y_acceleration, 'LineWidth', LineWidth);
+
+
+%% Numerator  constant 5625 has changed by 5% 
+numerator=[11.88 4.977 539.6 129.9 5625*1.05];
+denominator=[1 1.169 50.3 45.94 685.3 391.7 1952];
+Origianl_System_plus_five_percent=tf(numerator, denominator);
+y_position_plus_5_percent=lsim(Origianl_System_plus_five_percent, U_ff, t);
+y_position_plus_5_percent=100.*y_position_plus_5_percent;
+    %% Differentiate y_position twice into y_acceleration
+for i=1:length(t)-1
+    y_velocity_plus_5_percent(i)=y_position_plus_5_percent(i+1)-y_position_plus_5_percent(i);
+end
+
+for i=1:length(t)-2
+    y_acceleration_plus_5_percent(i)=y_velocity_plus_5_percent(i+1)-y_velocity_plus_5_percent(i);
+end
+plot(y_acceleration_plus_5_percent, 'LineWidth', LineWidth);
+
+%% Numerator  constant 5625 has changed by -5% 
+numerator=[11.88 4.977 539.6 129.9 5625*0.95];
+denominator=[1 1.169 50.3 45.94 685.3 391.7 1952];
+Origianl_System_minus_five_percent=tf(numerator, denominator);
+y_position_minus_5_percent=lsim(Origianl_System_minus_five_percent, U_ff, t);
+y_position_minus_5_percent=100.*y_position_minus_5_percent;
+    %% Differentiate y_position twice into y_acceleration
+for i=1:length(t)-1
+    y_velocity_minus_5_percent(i)=y_position_minus_5_percent(i+1)-y_position_minus_5_percent(i);
+end
+
+for i=1:length(t)-2
+    y_acceleration_minus_5_percent(i)=y_velocity_minus_5_percent(i+1)-y_velocity_minus_5_percent(i);
+end
+plot(y_acceleration_minus_5_percent, 'LineWidth', LineWidth);
+set(gcf,'Position',[100 100 1000 750])
+% lgd=legend('voltage input (u_f_f)', 'y_d', 'y acceleration', 'y_+_5_%', 'y_-_5_%');
+% lgd.FontSize=20;
+% hold off
+
+%% PID Feedback controller
+Kp=0.75;
+Ki=0.5;
+Kd=0.5;
+H=[1];
+Controller=pid(Kp,Ki,Kd);
+Origianl_System_minus_five_percent_PID=feedback(Controller*Origianl_System_minus_five_percent,H);
+y_position_minus_5_percent_PID=lsim(Origianl_System_minus_five_percent_PID, U_ff, t);
+y_position_minus_5_percent_PID=100.*y_position_minus_5_percent_PID;
+    %% Differentiate y_position twice into y_acceleration
+for i=1:length(t)-1
+    y_velocity_minus_5_percent_PID(i)=y_position_minus_5_percent_PID(i+1)-y_position_minus_5_percent_PID(i);
+end
+
+for i=1:length(t)-2
+    y_acceleration_minus_5_percent_PID(i)=y_velocity_minus_5_percent_PID(i+1)-y_velocity_minus_5_percent_PID(i);
+end
+plot(y_acceleration_minus_5_percent_PID, 'LineWidth', LineWidth);
+set(gcf,'Position',[100 100 1000 750])
+lgd=legend('voltage input (u_f_f)', 'y_d', 'y acceleration', 'y_+_5_%', 'y_-_5_%', '-5% with PID');
+lgd.FontSize=20;
