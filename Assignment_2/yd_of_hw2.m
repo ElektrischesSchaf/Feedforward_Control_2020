c90990a662d8891d714ab5a793ff3f147eef39bb
ElektrischesSchaf <vertigo1958psycho1960@gmail.com>
2020-03-27 12:40:19 +0800

    HW2

---
+++ Assignment_2/yd_of_hw2.m
@@ -0,0 +1,29 @@
+clear all; close all;
+
+% defining the desired output (and filtering it twice)
+tin = 1; tup =3;  tf = 8; delt = 0.001; ymax = 10;
+
+ramp = ymax/(tup-tin);
+t1 = 0:delt:tin; 
+t2=max(t1)+delt:delt:tup;
+t3=max(t2)+delt:delt:tf;
+y0 = zeros(size(t1));
+y1 = ramp*(t2-max(t1));
+y2 = max(y1)*ones(size(t3));
+t = 0:delt:tf; y = [y0 y1 y2];
+
+% filtering the desired signal 
+Wf = 1; % choose a filter break frequency in Hz
+num = [Wf*2*pi]; den = [1 (Wf*2*pi)]; % first order filter
+[Af,Bf,Cf,Df] = tf2SS(num,den);
+Sys_f = ss(Af,Bf,Cf,Df); % first order filter system
+Sysf = Sys_f*Sys_f*Sys_f*Sys_f*Sys_f; % fifth order filter
+[yd,xtemp]= lsim(Sysf,y,t); % filter the desired signal
+figure(1);
+subplot(211), plot(t,y); 
+axis([0 8 -1 11]);
+xlabel('time(s)'); ylabel('y (unfiltered)')
+subplot(212), plot(t,yd); 
+xlabel('time(s)'); ylabel('yd (filtered)')
+axis([0 8 -1 11]);
+
